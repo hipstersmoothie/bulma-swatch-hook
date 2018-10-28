3cf12fdfa5210f012a8cafdec9ebac7c44b5370d
Andrew Lisowski <andrew_lisowski@intuit.com>
2018-10-27 22:01:31 -0700

    try this

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -34,7 +34,8 @@ module.exports = ({ mode } = {}) => ({
       chunkFilename: '[id].css'
     }),
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, '/src/index.html')
+      template: path.join(__dirname, '/src/index.html'),
+      publicPath: mode === 'development' ? '/' : '/bulma-swatch-hook/'
     })
   ],
   devServer: {
